96bbd39b56267f1b4191bd984271f827fa7c1bce
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-04-26 17:03:08 -0400

    Clean up launcher.json

--- docker/03_run_docker.sh
+++ docker/03_run_docker.sh
@@ -1,12 +1,13 @@
-docker run \
-  --rm \
+docker run --rm \
+  -it \
   -p 9000:80 \
   -v "$(pwd)"/models:/home/ubuntu/sandtank/models \
   --mount type=bind,source="$(pwd)"/launcher.json,target=/opt/launcher/config.json \
   --mount type=bind,source="$(pwd)"/launcher.json,target=/opt/launcher/config-template.json \
+  --mount type=bind,source="$(pwd)"/logs,target=/logs \
   sandtank-ml 
 
 
-#  -it \
-#  --entrypoint /bin/bash \
+
+
 
