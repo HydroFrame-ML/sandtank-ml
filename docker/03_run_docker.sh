ab47216f918673ee82372ba1d14ac08a516067f2
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-04-26 19:30:49 -0400

    Add launcher config in Dockerfile

--- docker/03_run_docker.sh
+++ docker/03_run_docker.sh
@@ -3,8 +3,7 @@ docker run --rm \
   -p 9000:80 \
   -e SERVER_NAME="192.168.0.78:9000" \
   -e PROTOCOL="ws" \
-  -v "$(pwd)"/../models:/home/ubuntu/sandtank/models \
-  --mount type=bind,source="$(pwd)"/launcher.json,target=/opt/launcher/config-template.json \
+  --mount type=bind,source="$(pwd)"/../models,target=/home/ubuntu/sandtank/models \
   --mount type=bind,source="$(pwd)"/logs,target=/logs \
   sandtank-ml 
 
