470a24eee09defc12d63010b7aea06620da4c66b
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-04-28 10:12:41 -0600

    more cleanup

--- deploy/03_run_docker.sh
+++
@@ -1,8 +0,0 @@
-docker run --rm \
-  -it \
-  -p 9000:80 \
-  -e SERVER_NAME="192.168.0.78:9000" \
-  -e PROTOCOL="ws" \
-  --mount type=bind,source="$(pwd)"/../data,target=/opt/sandtank-ml/data \
-  sandtank-ml 
-
